33eff54a5eebb187140934c53cb0e08a06873289
umaircoder00 <arfeenumair93@gmail.com>
2026-08-14 21:54:09 +0500

    second project

---
+++ my-first-project/src/App.jsx
@@ -0,0 +1,169 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='main'>
+      <nav className='navbar'>
+    <h1>Music World</h1>
+    <div className='button'>
+        <button>login</button>
+      <button>sign up</button>
+      </div>
+</nav>
+
+<div className="banner">
+     <div className="typing-wrapper">
+        <h1 className="typing-text">Welcome to the music world .....</h1>
+         <img 
+    
+          src="https://www.freeiconspng.com/uploads/black-disco-cd-music-icon-png-6.png" 
+          alt="Logo" 
+          className="rotating-logo" 
+          
+        />
+
+      </div>
+</div>
+<div className='card-section'>
+  <div className='card'>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0Axxh25A3M-cBOzX-QH0OCMkiRMzOvyPTbd2RpFgh6g&s=10" alt="" />
+    <h1>jhanjhar</h1>
+    <div className='icon'>
+      <i class="fa-solid fa-less-than"></i>
+        <i class="fa-solid fa-pause"></i>
+        <i class="fa-solid fa-greater-than"></i>
+    </div>
+        <hr />
+  </div>
+
+   <div className='card'>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1y0gfGSPnqTfXZAgJ_7wPAnZ3Kz1YF0HlqpTiW6Bgig&s=10" alt="" />
+    <h1>Arzu</h1>
+    <div className='icon'>
+      <i class="fa-solid fa-less-than"></i>
+        <i class="fa-solid fa-pause"></i>
+        <i class="fa-solid fa-greater-than"></i>
+    </div>
+        <hr />
+  </div>
+
+   <div className='card'>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg2yV4iJ0rTgdDfxg_uuG1Km0gVSa3rV2XNk5mSD7nUA&s=10" alt="" />
+    <h1>sheikh</h1>
+    <div className='icon'>
+      <i class="fa-solid fa-less-than"></i>
+        <i class="fa-solid fa-pause"></i>
+        <i class="fa-solid fa-greater-than"></i>
+    </div>
+        <hr />
+  </div>
+
+   <div className='card'>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToLwRbL1Ef9HMMMPs5LKM47sMW-pNaphrKIQQTM89P3A&s" alt="" />
+    <h1>Husn</h1>
+    <div className='icon'>
+      <i class="fa-solid fa-less-than"></i>
+        <i class="fa-solid fa-pause"></i>
+        <i class="fa-solid fa-greater-than"></i>
+    </div>
+        <hr />
+  </div>
+
+   <div className='card'>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmHVKn_gGkS2QPA5KP9phgsgTI_FhEtWwIr1mgCPkkYQ&s=10" alt="" />
+    <h1>kahani</h1>
+    <div className='icon'>
+      <i class="fa-solid fa-less-than"></i>
+        <i class="fa-solid fa-pause"></i>
+        <i class="fa-solid fa-greater-than"></i>
+    </div>
+        <hr />
+  </div>
+
+   <div className='card'>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGwLKBHa3IibfLZyzUsRlwCper4rQ4cbazQJr8q0J6rg&s=10" alt="" />
+    <h1>khat</h1>
+    <div className='icon'>
+      <i class="fa-solid fa-less-than"></i>
+        <i class="fa-solid fa-pause"></i>
+        <i class="fa-solid fa-greater-than"></i>
+    </div>
+        <hr />
+  </div>
+
+   <div className='card'>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx9pg9XlTJL2R-1V8xRdt42xKla4SwR4tM6_koFa2mTg&s=10" alt="" />
+    <h1>taare</h1>
+    <div className='icon'>
+      <i class="fa-solid fa-less-than"></i>
+        <i class="fa-solid fa-pause"></i>
+        <i class="fa-solid fa-greater-than"></i>
+    </div>
+        <hr />
+  </div>
+
+
+   <div className='card'>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQojRlHEj0Ju3wgplZeStNhnr09E9OrJdrGRiVGS2CBBg&s=10" alt="" />
+    <h1>Wavy</h1>
+    <div className='icon'>
+      <i class="fa-solid fa-less-than"></i>
+        <i class="fa-solid fa-pause"></i>
+        <i class="fa-solid fa-greater-than"></i>
+    </div>
+        <hr />
+  </div>
+
+
+   <div className='card'>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoCgiu7v372boh8rRcQ7jUbGxVb4mlC-QFh-SYDNbsJw&s=10" alt="" />
+    <h1>Khasara</h1>
+    <div className='icon'>
+      <i class="fa-solid fa-less-than"></i>
+        <i class="fa-solid fa-pause"></i>
+        <i class="fa-solid fa-greater-than"></i>
+    </div>
+        <hr />
+  </div>
+
+
+   <div className='card'>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZnQJpCvgD1pZRrjlfeinX5g-XSPywsLtk1NsuOqrDAA&s=10" alt="" />
+    <h1>Paroosi</h1>
+    <div className='icon'>
+      <i class="fa-solid fa-less-than"></i>
+        <i class="fa-solid fa-pause"></i>
+        <i class="fa-solid fa-greater-than"></i>
+    </div>
+        <hr />
+  </div>
+
+
+   <div className='card'>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzLroE9MEXErkR7xBrPaaLhzVdpdzEQPqGeoUyegYodQ&s=10" alt="" />
+    <h1>Adat</h1>
+    <div className='icon'>
+      <i class="fa-solid fa-less-than"></i>
+        <i class="fa-solid fa-pause"></i>
+        <i class="fa-solid fa-greater-than"></i>
+    </div>
+        <hr />
+  </div>
+
+   <div className='card'>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXAv5vb9b6RDbzdgHF2_bkT9CVmj6DUMXK_TDDPSU52w&s=10" alt="" />
+    <h1>Softly</h1>
+    <div className='icon'>
+      <i class="fa-solid fa-less-than"></i>
+        <i class="fa-solid fa-pause"></i>
+        <i class="fa-solid fa-greater-than"></i>
+    </div>
+        <hr />
+  </div>
+</div>
+    </main>
+  )
+}
+
+export default App
